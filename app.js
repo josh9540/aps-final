cb9a4385e6d94374827d2715056d716594fdedf0
ayushkharkia3 <ayushkharkia3@gmail.com>
2020-05-24 13:09:19 +0530

    first commit

--- app.js
+++ app.js
@@ -84,5 +84,5 @@ app.use((error, req, res, next) => {
 });
 
 mongoose.connect(MONGODB_URI, { useCreateIndex: true, useNewUrlParser: true, useUnifiedTopology: true }).then(result => {
-    app.listen(3000)
+    app.listen(3000 || process.env.PORT)
 }).catch(err => { console.log(err) })
